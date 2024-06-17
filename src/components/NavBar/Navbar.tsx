fadfc650886bc0aeba8d7d6c344258b7ce4d1ff4
mywus <109961342+mywus@users.noreply.github.com>
2024-06-17 22:42:19 +0300

    added material ui, and created simple navbar that only navigates to root, table

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Navbar: React.FC = () => {
   return (
-    <div>
-     Navbar
-    </div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6">
+          <Link to="/" style={{ color: 'white', textDecoration: 'none' }}>
+            Database Management
+          </Link>
+        </Typography>
+      </Toolbar>
+    </AppBar>
   );
 };
 
