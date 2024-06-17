611f60e6d7cd78738fe75eb9ce970d2003fd91b7
mywus <109961342+mywus@users.noreply.github.com>
2024-06-17 22:50:21 +0300

    created db table component, which will be used in the db list page later

--- src/components/DatabaseTable/DatabaseTable.tsx
+++ src/components/DatabaseTable/DatabaseTable.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+import { Database } from '../../types/Database';
 
-const DatabaseTable: React.FC = () => {
+interface DatabaseTableProps {
+  databases: Database[];
+}
+
+const DatabaseTable: React.FC<DatabaseTableProps> = ({ databases }) => {
+  const navigate  = useNavigate();
+
+  const handleRowClick = (id: string) => {
+    // user will be navigated to the specific db detail page
+    navigate(`/database/${id}`);
+  };
 
   return (
-    <div>
-     DatabaseTable
-    </div>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Database Name</TableCell>
+          <TableCell>Username</TableCell>
+          <TableCell>Type</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {databases.map((db) => (
+          <TableRow key={db.id} onClick={() => handleRowClick(db.id)} style={{ cursor: 'pointer' }}>
+            <TableCell>{db.name}</TableCell>
+            <TableCell>{db.username}</TableCell>
+            <TableCell>{db.type}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
-}
+};
 
 export default DatabaseTable;
