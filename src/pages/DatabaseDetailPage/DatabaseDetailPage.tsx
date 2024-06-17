0cd7350f5bd4946187adb5cdacd596b742b6b23b
mywus <109961342+mywus@users.noreply.github.com>
2024-06-17 23:00:16 +0300

    implemented list page and detail page, roughly, need to fix css, implment submit dialog, implement proper loading states with loaders and error handling ui

--- src/pages/DatabaseDetailPage/DatabaseDetailPage.tsx
+++ src/pages/DatabaseDetailPage/DatabaseDetailPage.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { Container, Typography } from '@mui/material';
+import { Database } from '../../types/Database';
 
 const DatabaseDetailPage: React.FC = () => {
+  const { id } = useParams<{ id: string }>();
+  const [database, setDatabase] = useState<Database | null>(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:4000/databases/${id}`)
+      .then(response => setDatabase(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, [id]);
+
+  if (!database) {
+    return <Typography>Loading...</Typography>;
+  }
+
   return (
-    <div>
-     DatabaseDetailPage
-    </div>
+    <Container>
+      <Typography variant="h4">{database.name}</Typography>
+      <Typography>URL: {database.url}</Typography>
+      <Typography>Username: {database.username}</Typography>
+      <Typography>Type: {database.type}</Typography>
+    </Container>
   );
 };
 
--- src/pages/DatabaseListPage/DatabaseListPage.tsx
+++ src/pages/DatabaseListPage/DatabaseListPage.tsx
@@ -1,10 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
+import axios from 'axios';
+import DatabaseTable from '../../components/DatabaseTable/DatabaseTable';
+import DatabaseForm from '../../components/DatabaseForm/DatabaseForm';
+import { Database } from '../../types/Database';
 
 const DatabaseListPage: React.FC = () => {
+  const [databases, setDatabases] = useState<Database[]>([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/databases')
+      .then(response => setDatabases(response.data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+
 
   return (
-    <div>
-     DatabaseListPage
-    </div>
+    <Container>
+      <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
+        +
+      </Button>
+      <DatabaseTable databases={databases} />
+      <Dialog open={open} onClose={() => setOpen(false)}>
+        <DialogTitle>Add New Database</DialogTitle>
+        <DialogContent>
+          <DatabaseForm /> {/*@TODO handle on submit*/}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setOpen(false)} color="primary">
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Container>
   );
 };
 
